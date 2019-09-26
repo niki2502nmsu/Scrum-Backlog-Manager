f4d8526003d7e4079d70bf2c2829db2752e936d8
Sainikhitha Thooti <55858247+niki2502nmsu@users.noreply.github.com>
2019-09-26 16:35:33 -0600

    Create DashBoard.js

---
+++ components/DashBoard/DashBoard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function Dashboard() {
+  return (
+    <div>
+      <h3>
+        <FontAwesomeIcon icon="tachometer-alt" size="lg" /> Dashboard
+      </h3>
+    </div>
+  );
+}
+
+export default Dashboard;
