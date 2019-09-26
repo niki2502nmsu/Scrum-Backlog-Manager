d647a6795cd78c864cfcee7a806ce30fc25bb4d1
Sainikhitha Thooti <55858247+niki2502nmsu@users.noreply.github.com>
2019-09-26 16:31:40 -0600

    Create Home.js

---
+++ views/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import TopBar from "../components/TopBar/TopBar";
+import NavBar from "../components/NavBar/NavBar";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <TopBar />
+      </div>
+      <div>
+        <NavBar />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
